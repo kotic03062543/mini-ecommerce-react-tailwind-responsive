9cbb8e4f9f7951001ad7222b8279851c9c714b38
Wanchaloem <wanchaloem.phr@axonstech.com>
2025-05-13 14:14:18 +0700

    fix(app) : fix the issue page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import CardDog from "./components/dog/CardDogView";
 import Profile from "./components/dog/Profile";
+import Button from "./components/utils/Button";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<h1>HOME</h1>} />
+        <Route
+          path="/"
+          element={
+            <Button
+              text="Go to Card"
+              href="/card"
+              className="bg-blue-500 text-white rounded hover:bg-blue-600 w-full py-3 mt-3"
+              onClick={() => {}}
+            />
+          }
+        />
         <Route path="/card" element={<CardDog />} />
         <Route path="/profile/:id" element={<Profile />} />
         <Route path="*" element={<h1>Page not found</h1>} />
